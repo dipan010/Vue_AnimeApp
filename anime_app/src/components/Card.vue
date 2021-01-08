86a0d7307194ba571b3085b52cbfe10120e8dd25
Dipan Ghosh <dipan.ghosh@hotmail.com>
2021-01-09 00:25:02 +0530

    cardComponent created

---
+++ anime_app/src/components/Card.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="card">
+		<a :href="anime.url" target="_blank">
+			<img 
+				:src="anime.image_url" 
+				:alt="anime.title + ' Poster'" 
+			/>
+			<h3>{{ anime.title }}</h3>
+		</a>
+	</div>
+</template>
+
+<script>
+export default {
+	props: ['anime']
+}
+</script>
